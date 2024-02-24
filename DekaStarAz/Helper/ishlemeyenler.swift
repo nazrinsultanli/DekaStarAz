2e5762155677a005824ef126e2b3478b0b1e86c3
nazrinsultanli <nazrindolkhanova@gmail.com>
2024-02-25 00:57:15 +0400

    test

---
+++ DekaStarAz/Helper/ishlemeyenler.swift
@@ -0,0 +1,15 @@
+//
+//  ishlemeyenler.swift
+//  DekaStarAz
+//
+//  Created by Nazrin Sultanlı on 25.02.24.
+//
+
+import Foundation
+// favTabinda mal olmayanda mal ypxdu sozu gorunmur
+// elave eden kimi fav-a gorunmurdu
+//dil meselesi
+//cordinator yoxdu
+// initilize elemek
+//extension-lara cixarmaq meselen label-leri
+
